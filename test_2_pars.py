370d91958a305cd730de10bc7ce62fdd32b3f3ad
VladislavLipkinGit <lipkin1993vladislav@gmail.com>
2023-08-14 20:36:26 +0300

    no ready2

--- test_2_pars.py
+++ test_2_pars.py
@@ -22,8 +22,20 @@ def get_data(url):
     for article in articles:
         project_url = article.find("div", class_="item__inner").find("a").get("href")
         project_urls.append(project_url)
-    
-    for project_url in project_urls:
+
+    count = 0
+    for project_url in project_urls[0:1]:
+        count += 1
         req = requests.get(project_url, headers)
+        project_name = "startups" + str(count)
+
+        with open(f"data/{project_name}.html", "w") as file:
+            file.write(req.text)
+    
+        with open(f"data/{project_name}.html") as file:
+            src = file.read()
+        
+        soup = BeautifulSoup(src, "lxml")
+        project_data = soup.find("div", class_="l-base__col__main")
         
 get_data("https://trends.rbc.ru/trends/tag/educational_startups") 
